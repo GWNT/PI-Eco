c3037c9712ac4a6fd0df60fcdf2205481753e690
pdrZzZz <pedrocursos2211@gmail.com>
2024-09-01 23:34:39 -0300

    td perfect na movement tchau

--- Assets/pedro/Script/PlayerController.cs
+++ Assets/pedro/Script/PlayerController.cs
@@ -22,6 +22,12 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
 
+        if(Input.GetAxisRaw("Horizontal")==1 || Input.GetAxisRaw("Horizontal")==-1 || Input.GetAxisRaw("Vertical")==1 || Input.GetAxisRaw("Vertical")==-1)
+        {
+            animator.SetFloat("LastHorizontal", Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("LastVertical", Input.GetAxisRaw("Vertical"));
+        }
+
         movement = movement.normalized;
 
         Debug.Log($"Movement: {movement}, Speed: {movement.sqrMagnitude}");
