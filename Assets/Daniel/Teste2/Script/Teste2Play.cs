dde68b7d93b5026ea9dddec3d3a76ea2d8bea67c
DanMCosta <danielmartinscosta.2024@gmail.com>
2024-09-06 23:27:54 -0300

    Disparo de flecha
    Personagem disparando flecha já com animação. Para os "animadores" de plantão meu Boa noite. Agradecimento especial pro melhor professor que existe, isso mesmo: ChatGPT.

--- Assets/Daniel/Teste2/Script/Teste2Play.cs
+++ Assets/Daniel/Teste2/Script/Teste2Play.cs
@@ -5,16 +5,25 @@ using UnityEngine.InputSystem;
 
 public class Teste2Play : MonoBehaviour
 {
+    // Variaveis do player
     public Animator _anim;
     public float _speed;
     Rigidbody2D _rb;
     [SerializeField] Vector2 _move;
+    private Vector2 _lastMoveDirection; // armazena a última direção válida
     public bool _andando;
+
+    // Variaveis da flecha
+    public GameObject _arrowPrefab; // Prefab da flecha
+    public Transform _arrowSpawnPoint; // ponto de spanw da flecha
+    public float _arrowSpeed = 10f; // velocidade da flecha (acho que tá obvio pelo nome, mas né...)
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        Debug.Log("Special thanks to ChatGPT ;)");
+        Debug.Log("Botão esquerdo do mouse para disparar flechas.");
     }
 
     // Update is called once per frame
@@ -24,6 +33,7 @@ public class Teste2Play : MonoBehaviour
 
         if(_andando)
         {
+            _lastMoveDirection = _move.normalized; // atualiza a última direção válida
             _anim.SetFloat("Horizontal", _move.x);
             _anim.SetFloat("Vertical", _move.y);
         }
@@ -38,5 +48,63 @@ public class Teste2Play : MonoBehaviour
     public void SetMove(InputAction.CallbackContext value)
     {
         _move = value.ReadValue<Vector3>();
+    }
+
+    public void SetAttack(InputAction.CallbackContext value)
+    {
+        if(value.performed)
+        {
+            _anim.SetTrigger("Attack");
+            ShootArrow();
+        }
+    }
+
+    void ShootArrow()
+    {
+        // Instancia a flecha na posição do ponto de spawn
+        GameObject arrow = Instantiate(_arrowPrefab, _arrowSpawnPoint.position, Quaternion.identity);
+
+        // Calcula a direção da flecha com base no movimento atual do player
+        Vector2 shootDirection = _move == Vector2.zero ? _lastMoveDirection : _move.normalized;
+
+        // remover disparos na diagonal
+        if (Mathf.Abs(shootDirection.x) > Mathf.Abs(shootDirection.y))
+        {
+            // Se o movimento no eixo X for maior que no Y, travamos no eixo X
+            shootDirection = new Vector2(Mathf.Sign(shootDirection.x), 0); // dispara para os lados
+        }
+        else
+        {
+            // Se o movimento no eixo Y for maior ou igual, travamos no eixo Y
+            shootDirection = new Vector2(0, Mathf.Sign(shootDirection.y)); // dispara pra cima ou baixo
+        }
+        
+
+        // Rotaciona a flecha na direção do movimento
+        float angle = Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg;
+
+        // ajuste de rotação da flecha
+        if (shootDirection.x == 0 && shootDirection.y > 0) // Flecha apontando para cima
+        {
+            angle = 0;
+        }
+        else if(shootDirection.x == 0 && shootDirection.y < 0) // Flecha apontando para baixo
+        {
+            angle = 180;
+        }
+        else if(shootDirection.x > 0 && shootDirection.y == 0) // Flecha apontando para a direita
+        {
+            angle = -90;
+        }
+        else if(shootDirection.x < 0 && shootDirection.y == 0) // Flecha apontando para a esquerda
+        {
+            angle = 90;
+        }
+
+        arrow.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        // Aplica a força para mover a flecha
+        Rigidbody2D arrowRb = arrow.GetComponent<Rigidbody2D>();
+        arrowRb.velocity = shootDirection * _arrowSpeed;
     }
 }
